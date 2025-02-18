e07ef13505355e067f39de4fef128b2c23af15e9
Rhuan Guilherme <rhuan23032004@gmail.com>
2025-02-18 18:41:14 -0300

    Ajusta caso de uso para deleção de usuário. Feita a verificação da existencia do usuário antes da exclusão

--- src/use-cases/delete-user.ts
+++ src/use-cases/delete-user.ts
@@ -1,9 +1,16 @@
 import { UserRepositoryInterface } from '@/repositories/user-repository-interface';
+import { ResourceNotFoundError } from './exceptions/resource-not-foud-error';
 
 export class DeleteUserUseCase {
   constructor(private userRepository: UserRepositoryInterface) {}
 
   async execute(id: string): Promise<void> {
+    const userExists = await this.userRepository.findById(id);
+
+    if (!userExists) {
+      throw new ResourceNotFoundError();
+    }
+
     await this.userRepository.deleteUser(id);
   }
 }
--- src/use-cases/tests/delete-user.spec.ts
+++ src/use-cases/tests/delete-user.spec.ts
@@ -1,6 +1,7 @@
 import { beforeAll, describe, expect, test } from 'vitest';
 import { InMemoryUserRepository } from '@/repositories/in-memory-repository/in-memory-user-repository';
 import { DeleteUserUseCase } from '../delete-user';
+import { ResourceNotFoundError } from '../exceptions/resource-not-foud-error';
 
 let repository: InMemoryUserRepository;
 let sup: DeleteUserUseCase;
@@ -30,9 +31,19 @@ describe('Teste deleção de usuários.', () => {
 
     await sup.execute(userCreate.id);
 
-    console.log(repository.user);
-
     expect(repository.user.length).toEqual(2);
     expect(repository.user[2]).toBe(undefined);
+  });
+
+  test('Não deve ser possível excluir usuário inexistente no banco de dados.', async () => {
+    await repository.create({
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      password: 'password123',
+    });
+
+    await expect(() => sup.execute('1231241')).rejects.instanceOf(
+      ResourceNotFoundError
+    );
   });
 });
