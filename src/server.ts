8ce4062fbe62a013d5ce8593b14ca1149802a7f0
Rhuan Guilherme <rhuan23032004@gmail.com>
2025-03-04 15:28:22 -0300

    Ajustando host para solucionar problema no deploy

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { env } from './env';
 app
   .listen({
     port: env.PORT,
+    host: '0.0.0.0', // to allow connections from outside the container
   })
   .then(() =>
     console.log(`🚀 Server listening in http://localhost:${env.PORT}`)
